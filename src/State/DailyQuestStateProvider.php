c8899c01a254de4997eecd1728fd63fa642e8bec
Seelegen <martin@tdrapied>
2024-03-05 10:41:43 +0100

    So much things

---
+++ src/State/DailyQuestStateProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\CollectionOperationInterface;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use App\ApiResource\DailyQuest;
+use App\Enum\DailyQuestStatusEnum;
+
+class DailyQuestStateProvider implements ProviderInterface
+{
+    /**
+     * @throws \Exception
+     */
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
+    {
+        if ($operation instanceof CollectionOperationInterface) {
+            return $this->createQuests();
+        }
+
+        $quests = $this->createQuests();
+
+        return $quests[$uriVariables['dayString']] ?? null;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function createQuests(): array
+    {
+        $quests = [];
+        for ($i = 0; $i < 50; $i++) {
+            $quest = new DailyQuest(new \DateTimeImmutable(sprintf('- %d days', $i)));
+            $quest->questName = sprintf('Quest %d', $i);
+            $quest->description = sprintf('Description %d', $i);
+            $quest->difficultyLevel = $i % 10;
+            $quest->status = $i % 2 === 0 ? DailyQuestStatusEnum::ACTIVE : DailyQuestStatusEnum::COMPLETED;
+
+            $quests[$quest->getDayString()] = $quest;
+        }
+
+        return $quests;
+    }
+}
---
+++ src/State/DragonTreasureStateProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+
+class DragonTreasureStateProvider implements ProviderInterface
+{
+    public function __construct(private ProviderInterface $itemProvider)
+    {
+    }
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
+    {
+        dd($operation);
+    }
+}
